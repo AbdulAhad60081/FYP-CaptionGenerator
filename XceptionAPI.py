e175e1e64b403a49b4e43519b49f3444e35c536e
Abdul Ahad <58343607+AbdulAhad60081@users.noreply.github.com>
2021-01-04 15:58:58 +0500

    Update XceptionAPI.py

--- XceptionAPI.py
+++ XceptionAPI.py
@@ -3,7 +3,7 @@
 
 # In[ ]:
 
-
+import os
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.applications.xception import Xception
@@ -12,56 +12,53 @@ from pickle import load
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import argparse
+from keras.models import Model
+from keras.preprocessing import image
+from keras.applications.xception import preprocess_input
+from flask_cors import CORS, cross_origin
 
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
-
-max_length = 34
-tokenizer = load(open("tokenizer.p","rb"))
-model = load_model('model_24.h5')
-# Define a flask app
 app = Flask(__name__)
-def preprocess(image_path):
-    # Convert all the images to size 299x299 as expected by the inception v3 model
-    img = image.load_img(image_path, target_size=(299, 299))
-    # Convert PIL image to numpy array of 3-dimensions
-    x = image.img_to_array(img)
-    # Add one more dimension
-    x = np.expand_dims(x, axis=0)
-    # preprocess the images using preprocess_input() from inception module
-    x = preprocess_input(x)
-    return x
+
+# cors = CORS(app, resources={r"/api/http://localhost:4200": {"origins": "http://localhost:4200"}})# api = Api(app)
+global wordtoix
+global ixtoword
+global model1
 
 
-def word_for_id(integer, tokenizer):
- for word, index in tokenizer.items():
-     if index == integer:
-         return word
- return None
 
-def generate_desc(model, tokenizer, photo, max_length):
-    in_text = 'start'
+wordtoix=load(open("wordtoix.p","rb"))
+ixtoword=load(open("ixtoword.p","rb"))
+model1=load_model('model_24.h5')
+
+
+# Define a flask app
+
+def generate_desc(model, photo, max_length):
+    in_text = 'startseq'
     for i in range(max_length):
-        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = [wordtoix[w] for w in in_text.split() if w in wordtoix]
         sequence = pad_sequences([sequence], maxlen=max_length)
-        pred = model.predict([photo,sequence], verbose=0)
-        pred = np.argmax(pred)
-        word = word_for_id(pred, tokenizer)
-        if word is None:
-            break
+        yhat = model.predict([photo,sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = ixtoword[yhat]
         in_text += ' ' + word
-        if word == 'end':
+        if word == 'endseq':
             break
-    return in_text
-
+    final = in_text.split()
+    final = final[1:-1]
+    final = ' '.join(final)
+    return final
 
 
 @app.route('/', methods=['GET'])
 def index():
     # Main page
-    return render_template('index.html)
+    return render_template('index.html')
 
 
 
@@ -71,23 +68,40 @@ def upload():
     if request.method == 'POST':
         # Get the file from post request
         f = request.files['file']
-
+        print('\n\n\n\n',f,'\n\n\n\n')
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         print(basepath)
+        print('\n\n\n\n',basepath,'\n\n\n\n')
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
-        print(file_path)
+        # print(file_path)
+        
+        print('\n\n\n\n file path ',file_path,'\n\n\n\n')
         f.save(file_path)
+        max_length = 34
         model= Xception(weights='imagenet')
         model_new = Model(model.input, model.layers[-2].output)
-        image = preprocess(image) # preprocess the image
-        fea_vec = model_new.predict(image) # Get the encoding vector for the image
-        fea_vec = np.reshape(fea_vec, fea_vec.shape[1])
-
-        description = generate_desc(model, tokenizer, photo, max_length)
+        
+        
+        img=Image.open(file_path)# Convert PIL image to numpy array of 3-dimensions
+        img=img.resize((299,299))
+        x = image.img_to_array(img)
+        # Add one more dimension
+        x = np.expand_dims(x, axis=0)
+        # preprocess the images using preprocess_input() from inception module
+        x = preprocess_input(x)
+        fea_vec = model_new.predict(x) # Get the encoding vector for the image
+        fea_vec = fea_vec.reshape((1,2048))
+        
+        description = generate_desc(model1, fea_vec, max_length)
         print("\n\n")
         print(description)
+
+
+
+        
+        
         
        
         # Process your result for human   
@@ -97,6 +111,10 @@ def upload():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+                           
+
 
 
                            
